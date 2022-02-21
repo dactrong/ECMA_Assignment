631875f8a209606c90550e62388fe79f99c21a92
dactrong <trongndph13723@fpt.edu.vn>
2022-02-21 12:01:18 +0700

    update cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -4,75 +4,188 @@ import { reRender } from "../utils";
 import { decreaseQuantity, increaseQuantity, removeItemInCart } from "../utils/cart";
 
 const CartPage = {
-  render() {
-    let cart = [];
-    if (localStorage.getItem('cart')) {
-      cart = JSON.parse(localStorage.getItem('cart'));
-    }
-    return /*html*/`
+render() {
+let cart = [];
+if (localStorage.getItem('cart')) {
+cart = JSON.parse(localStorage.getItem('cart'));
+}
+return /*html*/`
 
-        ${Header.render()}
-        <div class="flex flex-col">
-  <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-      <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-    
-        <table class="min-w-full divide-y divide-gray-200">
-          <thead class="bg-gray-50">
-            <tr>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Tên sản phẩm</th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Ảnh</th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Giá sản phẩm</th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Số lượng</th>
-              <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Role</th>
-              <th scope="col" class="relative px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody class="bg-white divide-y divide-gray-200">
-          ${cart.map(item =>/*html*/ `
-            <tr>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                  
-                  <div class="ml-4">
-                    <div class="text-sm font-medium text-gray-900">${item.name}</div>
-                  
-                  </div>
-                </div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="flex items-center">
-                  <div class="flex-shrink-0 h-20 w-36">
-                    <img class="h-20 w-36" src="${item.img}" alt="">
-                    
-                  </div>
-                 
-                </div>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap">
-                <div class="text-sm text-gray-900">${item.price}VNĐ</div>
-              
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap"> 
-                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full "> ${item.quantity}</span>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-              <button data-id="${item.id}" class="btn btn-increase">+</button>
-             <button data-id="${item.id}" class="btn btn-decrease">-</button>
-              </td>
-              <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                <a href="#" class="text-indigo-600 hover:text-indigo-900"><button data-id="${item.id}" class="btn btn-remove">Xóa</button></a>
-              </td>
-            </tr>
-            `).join('')}
-            <!-- More people... -->
-          </tbody>
-        </table>
-      </div>
+${Header.render()}
+<div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <form action="">
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Tên sản phẩm</th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Ảnh</th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Giá sản phẩm</th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Số lượng</th>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                </th>
+                                <th scope="col" class="relative px-6 py-3">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            ${cart.map(item =>/*html*/ `
+                            <tr>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center">
+
+                                        <div class="ml-4">
+                                            <div class="text-sm font-medium text-gray-900">${item.name}</div>
+
+                                        </div>
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div class="flex-shrink-0 h-20 w-36">
+                                            <img class="h-20 w-36" src="${item.img}" alt="">
+
+                                        </div>
+
+                                    </div>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="text-sm text-gray-900">${item.price}VNĐ</div>
+
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full ">
+                                        ${item.quantity}</span>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                    <button data-id="${item.id}"
+                                        class="btn btn-increase border p-2 bg-orange-500 text-white">+</button>
+                                    <button data-id="${item.id}"
+                                        class="btn btn-decrease border p-2 bg-lime-600 text-white">-</button>
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                    <a href="#" class="text-indigo-600 hover:text-indigo-900"><button
+                                            data-id="${item.id}" class="btn btn-remove">Xóa</button></a>
+                                </td>
+                            </tr>
+
+                            <!-- More people... -->
+                        </tbody>
+
+                    </table>
+                    <div class="mt-10 sm:mt-0">
+                        <div class="md:grid md:grid-cols-3 md:gap-6">
+                            <div class="md:col-span-1">
+                                <div class="px-4 sm:px-0">
+                                    <h3 class="text-lg font-medium leading-6 text-gray-900">${item.tong}</h3>
+                                    <p class="mt-1 text-sm text-gray-600">Use a permanent address where you can receive
+                                        mail.</p>
+                                </div>
+                            </div>
+                            <div class="mt-5 md:mt-0 md:col-span-2">
+
+                                <div class="shadow overflow-hidden sm:rounded-md">
+                                    <div class="px-4 py-5 bg-white sm:p-6">
+                                        <div class="grid grid-cols-6 gap-6">
+                                            <div class="col-span-6 sm:col-span-3">
+                                                <label for="first-name"
+                                                    class="block text-sm font-medium text-gray-700">First name</label>
+                                                <input type="text" name="first-name" id="first-name"
+                                                    autocomplete="given-name"
+                                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                            </div>
+
+                                            <div class="col-span-6 sm:col-span-3">
+                                                <label for="last-name"
+                                                    class="block text-sm font-medium text-gray-700">Last name</label>
+                                                <input type="text" name="last-name" id="last-name"
+                                                    autocomplete="family-name"
+                                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                            </div>
+
+                                            <div class="col-span-6 sm:col-span-4">
+                                                <label for="email-address"
+                                                    class="block text-sm font-medium text-gray-700">Email
+                                                    address</label>
+                                                <input type="text" name="email-address" id="email-address"
+                                                    autocomplete="email"
+                                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                            </div>
+
+                                            <div class="col-span-6 sm:col-span-3">
+                                                <label for="country"
+                                                    class="block text-sm font-medium text-gray-700">Country</label>
+                                                <select id="country" name="country" autocomplete="country-name"
+                                                    class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                                    <option>United States</option>
+                                                    <option>Canada</option>
+                                                    <option>Mexico</option>
+                                                </select>
+                                            </div>
+
+                                            <div class="col-span-6">
+                                                <label for="street-address"
+                                                    class="block text-sm font-medium text-gray-700">Street
+                                                    address</label>
+                                                <input type="text" name="street-address" id="street-address"
+                                                    autocomplete="street-address"
+                                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                            </div>
+
+                                            <div class="col-span-6 sm:col-span-6 lg:col-span-2">
+                                                <label for="city"
+                                                    class="block text-sm font-medium text-gray-700">City</label>
+                                                <input type="text" name="city" id="city" autocomplete="address-level2"
+                                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                            </div>
+
+                                            <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                                                <label for="region"
+                                                    class="block text-sm font-medium text-gray-700">State /
+                                                    Province</label>
+                                                <input type="text" name="region" id="region"
+                                                    autocomplete="address-level1"
+                                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                            </div>
+
+                                            <div class="col-span-6 sm:col-span-3 lg:col-span-2">
+                                                <label for="postal-code"
+                                                    class="block text-sm font-medium text-gray-700">ZIP / Postal
+                                                    code</label>
+                                                <input type="text" name="postal-code" id="postal-code"
+                                                    autocomplete="postal-code"
+                                                    class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                                        <button type="submit"
+                                            class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+                </form>
+                `).join('')}
+            </div>
+        </div>
     </div>
-  </div>
 </div>
         ${Footer.render()}
             
@@ -80,6 +193,7 @@ const CartPage = {
   },
   afterRender() {
     const btns = document.querySelectorAll('.btn');
+    const tong = price * quantity;
     btns.forEach(button => {
       button.addEventListener('click', function () {
         const id = button.dataset.id;
